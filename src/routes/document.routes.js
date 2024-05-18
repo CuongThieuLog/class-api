a7f6c30f47f924e7199c240ea96be783187c59c9
Cuong Thieu <thieutrancuong.log@gmail.com>
2024-05-18 15:41:05 +0700

    Document

--- src/routes/document.routes.js
+++ src/routes/document.routes.js
@@ -3,11 +3,16 @@ const DocumentController = require("../controllers/document.controller");
 const upload = require("../config/multerConfig");
 const auth = require("../middleware/auth.middleware");
 
-router.post("/all", auth, DocumentController.getAll);
-router.post("/:id", auth, DocumentController.getById);
+router.get("/all", auth, DocumentController.getAll);
+router.get("/get-by-id/:id", auth, DocumentController.getById);
 router.post("/create", auth, upload.single("file"), DocumentController.create);
-router.put("/:id", auth, upload.single("file"), DocumentController.update);
-router.delete("/:id", auth, DocumentController.remove);
+router.put(
+  "/update/:id",
+  auth,
+  upload.single("file"),
+  DocumentController.update
+);
+router.delete("/delete/:id", auth, DocumentController.remove);
 router.get("/download/:id", auth, DocumentController.download);
 
 module.exports = router;
